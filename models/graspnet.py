adc8b5fb0e093ffee420b3eb001f9183ca9a578b
Haozhe Wang <wang_haozhe@u.nus.edu>
2021-09-14 17:08:28 +0800

    added models directory

---
+++ models/graspnet.py
@@ -0,0 +1,22 @@
+from torch import nn
+import torch.nn.functional as F
+
+class GraspNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        #### TODO: Write the code for the model architecture below ####
+        
+        ################################################################
+
+    def forward(self, x):
+        #### TODO: Write the code for the model architecture below ####
+        
+        ################################################################
+        return x
+
+
+def get_graspnet():
+    
+    model = GraspNet()
+
+    return model
