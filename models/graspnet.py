7a26371cbe75f62fa928e65598fa2ef9e62cd502
Zhengshen Zhang <e0816481@u.nus.edu>
2021-09-14 17:25:03 +0800

    updated graspnet.py

--- models/graspnet.py
+++ models/graspnet.py
@@ -1,5 +1,7 @@
+import torch
 from torch import nn
 import torch.nn.functional as F
+from d2l import torch as d2l
 
 class GraspNet(nn.Module):
     def __init__(self):
@@ -8,11 +10,32 @@ class GraspNet(nn.Module):
         
         ################################################################
 
-    def forward(self, x):
+        net = nn.Sequential(
+
+                nn.Conv2d(3, 64, kernel_size=5, stride=2, padding=2), nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2, stride=2),
+
+                nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1), nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2, stride=2),
+
+                nn.Conv2d(128, 128, kernel_size=3, padding=1), nn.ReLU(),
+                nn.Conv2d(128, 128, kernel_size=3, padding=1), nn.ReLU(),
+                nn.Conv2d(128, 256, kernel_size=3, padding=1), nn.ReLU(),
+                nn.MaxPool2d(kernel_size=2, stride=2),
+                nn.Flatten(),
+
+                nn.Linear(12544, 512), nn.ReLU(),
+                nn.Dropout(p=0.5),
+                nn.Linear(512, 512), nn.ReLU(),
+                nn.Dropout(p=0.5),
+
+                nn.Linear(512, 5))
+
+    # def forward(self, x):
         #### TODO: Write the code for the model architecture below ####
         
         ################################################################
-        return x
+        # return x
 
 
 def get_graspnet():
